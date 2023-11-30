2e4227d2ab9bf023252eab89f026467665df95c4
JulianLu1028 <63889105+JulianLu1028@users.noreply.github.com>
2023-11-30 18:37:37 +0800

    login UI comment

--- login_module.py
+++ login_module.py
@@ -4,7 +4,7 @@ from tkinter import *
 import tkinter as tk
 import json
 
-login_success = False   #因為tkinter的執行是loop, 所以確認登入用的變數要放在外面
+login_success = False   #因為tkinter的執行是loop, 所以確認登入用的變數要放在外麵
 
 def start_login_interface():
 
@@ -13,34 +13,34 @@ def start_login_interface():
     window.title('網路撿紅點 - 登入')
 
     def user_log_in(event = None):
-        user_name = uname.get() #获取登录账号
-        user_pwd = password.get()#获取登录密码
+        user_name = uname.get() 
+        user_pwd = password.get()
 
         try:
-            with open('usr_info.json','r') as usr_file: #加载json文件
+            with open('usr_info.json','r') as usr_file: #load json doc
                 users_info = json.load(usr_file)
-        except FileNotFoundError :                      #如果文件为空 创建默认账号密码
+        except FileNotFoundError :                      #if json empty, create json
             with open('usr_info.json','w') as usr_file :
                 users_info = {'admin':'admin'}
-                json.dump(users_info,usr_file)          #把默认账号密码加载进json中
+                json.dump(users_info,usr_file)          #And create 'admin' account
 
-        if user_name in users_info :                    #判断账号是否存在
-            if user_pwd == users_info[user_name] :      #判断账号密码是否正确
-                tk.messagebox.showinfo('登入成功 ','歡迎您：'+user_name)  #正确则提示正确
+        if user_name in users_info :                    
+            if user_pwd == users_info[user_name] :      
+                tk.messagebox.showinfo('登入成功 ','歡迎您：'+user_name)  
 
                 global login_success    #因為要修改全域變數,要用global
                 login_success = True
 
-                window.destroy()  # 關閉 Tkinter 視窗
+                window.destroy()  
             else:
-                tk.messagebox.showerror('密碼錯誤')               #提示错误
-        elif user_name == ''  or user_pwd == '' :                     #判断账号密码是否为空
+                tk.messagebox.showerror('密碼錯誤')               #提示錯誤
+        elif user_name == ''  or user_pwd == '' :                     #判斷帳號密碼是否為空
             tk.messagebox.showerror('錯誤','帳號或密碼為空')            #提示
 
         else :
-            flag = tk.messagebox.askyesno('尚未註冊，是否要註冊') #如果账号存在提示是否注册
+            flag = tk.messagebox.askyesno('尚未註冊，是否要註冊') #如果帳號存在提示是否註冊
             if flag :
-                rgister() #调用注册函数
+                rgister() #調用註冊函數
 
 
     canvas = tk.Canvas(window,bg='purple',height=300,width=500)
@@ -49,7 +49,7 @@ def start_login_interface():
     canvas.place(x=0,y=0,anchor='nw')
 
 
-    #设置登录按钮
+    #設置登錄按鈕
     login=tk.Button(window,text='登入', width=8, height=2,command=user_log_in)
     login.place(y=416,x=240)
 
@@ -58,32 +58,32 @@ def start_login_interface():
     rg_repsd=tk.StringVar
 
     def user_register():
-            sign_name = rg_name.get()               #获取注册账号
-            sign_psw = rg_psd.get()                 #获取注册密码
-            pwd_confirm = rg_repsd.get()            #获取注册确认密码
+            sign_name = rg_name.get()               #獲取註冊帳號
+            sign_psw = rg_psd.get()                 #獲取註冊密碼
+            pwd_confirm = rg_repsd.get()            #獲取註冊確認密碼
 
             try:
-                with open('usr_info.json', 'r') as usr_file:     #加载json文件 存进usr_file中 with as自动关闭文件
-                    exist_usr_info = json.load(usr_file)         #加载usr_file 文件 中的数据赋值给exist_usr_info
-                    print(exist_usr_info)                        #这个用打印文件 用来调试 看看数据有没有存在
-            except FileNotFoundError:                            #如果数据为空 初始化一个空字典 
+                with open('usr_info.json', 'r') as usr_file:     #加載json文件 存進usr_file中 with as自動關閉文件
+                    exist_usr_info = json.load(usr_file)         #加載usr_file 文件 中的數據賦值給exist_usr_info
+                    print(exist_usr_info)                        #這個用打印文件 用來調試 看看數據有沒有存在
+            except FileNotFoundError:                            #如果數據為空 初始化一個空字典 
                 exist_usr_info = {}
 
-            if sign_name in exist_usr_info:                      #判断获取需要注册的账号是否存在
-                tk.messagebox.showerror('錯誤', '帳號已存在')     #如果存在则提示
-            elif sign_psw == '' or sign_name == '':              #如果账号密码为空则提示
+            if sign_name in exist_usr_info:                      #判斷獲取需要註冊的帳號是否存在
+                tk.messagebox.showerror('錯誤', '帳號已存在')     #如果存在則提示
+            elif sign_psw == '' or sign_name == '':              #如果帳號密碼為空則提示
                 tk.messagebox.showerror('錯誤', '帳號或密碼為空')  
-            elif sign_psw != pwd_confirm:                        #如果俩次密码输入不一致提示
-                tk.messagebox.showerror('錯誤', '密碼前後不一致')
+            elif sign_psw != pwd_confirm:                        #如果倆次密碼輸入不一緻提示
+                tk.messagebox.showerror('錯誤', '密碼前後不一緻')
             else:
-                exist_usr_info[sign_name] = sign_psw             #否则存入字典 
+                exist_usr_info[sign_name] = sign_psw             #否則存入字典 
                 with open('usr_info.json', 'w') as usr_file:      
                     json.dump(exist_usr_info, usr_file)       
                 tk.messagebox.showinfo('註冊成功')   
 
 
 
-    #注册topLevel窗口
+    #註冊topLevel窗口
     def rgister():
         global rg_name
         global rg_repsd
@@ -107,18 +107,18 @@ def start_login_interface():
 
 
 
-    #设置注册按钮
+    #設置註冊按鈕
     regiset=tk.Button(window,text='註冊', width=8, height=2,command=rgister)
     regiset.place(y=416,x=140)
 
     tk.Button(window,text='退出', width=8, height=2,command=sys.exit).place(y=460,x=190)
 
-    #账号标签及文本框
+    #帳號標籤及文本框
     Label(window,text='帳號：').place(y=350,x=140)
     uname=tk.Entry(window, width= 20)
     uname.place(y=350,x=175)
 
-    #密码标签及文本框
+    #密碼標籤及文本框
     Label(window,text='密碼：').place(y=385,x=140)
     password=tk.Entry(window, show='*', width= 20)
     password.place(y=385,x=175)
